a1398001907c8218b45cdbf3d313e730e94850a1
StarrFox <sammystarr7@gmail.com>
2019-01-28 17:30:10 -0600

    fixed snipe not showing in help command, added dbl event in prep

--- modules/events.py
+++ modules/events.py
@@ -1,10 +1,16 @@
 import discord
 import asyncio
+import dbl
 
 class events:
 
     def __init__(self, bot):
         self.bot = bot
+        try:
+            self.dbl_client = dbl.Client(self.bot, self.bot.settings['dbltoken'])
+            self.bot.loop.create_task(self.dbl())
+        except:
+            pass
 
     async def on_message(self, message):
         """Handles all incoming messages"""
@@ -20,6 +26,15 @@ class events:
 
     async def on_guild_remove(self, guild):
         self.bot.prefixes.pop(guild.id)
+
+    async def dbl(self):
+        """Updates DBl guild stats"""
+        while not self.bot.is_closed():
+            try:
+                await self.dbl_client.post_server_count()
+            except:
+                pass
+            await asyncio.sleep(1800)
 
 def setup(bot):
     bot.add_cog(events(bot))
