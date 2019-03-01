47ef7a0ca6d0f4e80eaac2c19f33120dc25b30c8
StarrFox <StarrFox6312@gmail.com>
2019-03-01 01:14:19 -0600

    Sql command

--- modules/owner.py
+++ modules/owner.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import asyncio
 from os import system
 import typing
+from tabulate import tabulate
 
 class owner(commands.Cog):
     """Owner commands"""
@@ -43,6 +44,31 @@ class owner(commands.Cog):
             await cog.stop_dbl()
             self.dbl_state = False
             return await ctx.send("Stopped Dbl tasks")
+
+#Modified from
+#https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L209-L250
+
+    @commands.command()
+    @commands.is_owner()
+    async def sql(self, ctx, *, entry: str):
+        """Sql command"""
+        is_multistatement = query.count(';') > 1
+        if is_multistatement:
+            strategy = self.bot.db.execute
+        else:
+            strategy = self.bot.db.fetch
+        try:
+            results = await strategy(query)
+        except Exception:
+            return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+        rows = len(results)
+        if is_multistatement or rows == 0:
+            return await ctx.send(f'`{dt:.2f}ms: {results}`')
+        tube = []
+        header = list(results[0].keys)
+        tube.append(list(i.values()) for i in results)
+        tab = tabulate(tube, header, tablefmt='fancy_grid')
+        await ctx.send(f"```py\n{tab}```")
 
 def setup(bot):
     bot.add_cog(owner(bot))
