472093fb4449c5d9b7d0d67d61ba442e57a243bb
StarrFox <sammystarr7@gmail.com>
2019-03-18 23:02:47 -0500

    why are you like this

--- modules/yes.py
+++ modules/yes.py
@@ -2,10 +2,7 @@ from discord.ext import commands
 
 class Yes(commands.Cog):
 
-<<<<<<< HEAD
     @commands.command()
-=======
->>>>>>> 6089ca0fba2cfcd678963f0bae31ff701b340b18
     async def epic(self, ctx):
         """Epic"""
         await ctx.send("Epic:sunglasses:")
