f541191b5b99f56dbe2f8cc78c6d0458fefd8d7a
StarrFox <StarrFox6312@gmail.com>
2019-03-23 03:13:39 -0500

    more helps

--- core.py
+++ core.py
@@ -118,9 +118,10 @@ class DiscordChan(commands.AutoShardedBot):
 
     async def load_mods(self):
         self.load_extension('jishaku')
+	self.load_extension(extras.helps.starrhelp) #Default help
         for ext in os.listdir('modules'):
             try:
-                if not ext.endswith(".py"):
+                if not ext.endswith(".py") or ext == "help.py": #temp fix
                     continue
                 self.load_extension(f"modules.{ext.replace('.py', '')}")
                 self.logger.info(f"Loaded {ext}")
