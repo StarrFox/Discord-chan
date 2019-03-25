876c531f391a8c999f9398dbbc5bac150fdecd8e
StarrFox <StarrFox6312@gmail.com>
2019-03-25 05:16:32 -0500

    bl command

--- setup.sql
+++ setup.sql
@@ -1,8 +1,3 @@
-CREATE TABLE IF NOT EXISTS blacklist (
-    userid BIGINT PRIMARY KEY,
-    reason TEXT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS prefixes (
     guild_id BIGINT,
     prefixes VARCHAR(1999)[]
