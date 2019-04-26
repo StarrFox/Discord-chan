fa54f01d54e4e37028c5bcc1f851e0a08532e870
StarrFox <sammystarr7@gmail.com>
2019-04-25 23:54:31 -0500

    Command logger update

--- cogs/logger.py
+++ cogs/logger.py
@@ -53,9 +53,12 @@ class logger(commands.Cog):
         if ctx.guild:
             g = ctx.guild.id
         log = f"Commandlog path={ctx.command.full_parent_name + ctx.command.name} g/c/u={g}/{ctx.channel.id}/{ctx.author.id}"
-        invoke = f"content={ctx.message.content}"
-        await self.log_channel.send(utils.block(log))
-        await self.log_channel.send(utils.block(invoke))
+        invoke = f"Content={ctx.message.content}"
+        if len(log+invoke)+8 >= 2000:
+            await self.log_channel.send(utils.block(log))
+            await self.log_channel.send(utils.block(invoke))
+        else:
+            await self.log_channel.send(utils.block(log+invoke))
         self.bot.logs.append(log+invoke)
 
     @commands.Cog.listener()
