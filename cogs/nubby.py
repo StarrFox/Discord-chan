a9212ec2585ed908ef33f8c1d092f223ae7f8127
StarrFox <StarrFox6312@gmail.com>
2019-05-16 13:40:54 -0500

    added timed check for twoweeks and cleaned up command

--- cogs/nubby.py
+++ cogs/nubby.py
@@ -1,4 +1,4 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 import discord
 import datetime
 from extras import utils
@@ -35,6 +35,8 @@ class nubby(commands.Cog):
             "You miserable ungrateful piss ants! How dare you insects go after me!",
             "Laval Prideland here, you guys know nothing. Contrary to what Justin says, I am much smarter than him, and possibly all of you here. I have every move that any of you can make against me mapped, predicted, and destroyed. Simpletons. Surely you believe I am a close friend of sir Decius? He shows me everything including the new mobile games that Kingsisle is making. I am not here for the riffraff of the Discords, I am only here for my holy lord, Nubby. I deeply hope he will show me the secrets of datamining and the Kingsisle games. In fact, I am the only one here who has a chance of getting that. None of you trolls know anything about me or the ethereal secrets of Wizard101. I applaud your arrogance and cunning, but certainly, you know of my wittiness? Any joke that you make me the butt of, will be reversed right back on you. Don't even bother, you will only be outwitted and humiliated in front of the good people of the Kingsisle community. As you ridicule me, I will ridicule you back in a thousand different ways that you cannot even comprehend. And no, I do not accept acts of sexual nature, to any of the female humans here who admire me, as I am celibate. Again, I must warn you, back down before you make fools out of yourselves against the minotaur of Kingsisle Entertainment, me. I will show no mercy on idiots such as the likes of you."
         ]
+        self.command_channel = self.bot.get_channel(442823830902145034)
+        self.check_twoweeks.start()
 
     async def cog_check(self, ctx):
         return ctx.guild == self.guild
@@ -49,9 +51,7 @@ class nubby(commands.Cog):
     async def laval(self, ctx):
         await ctx.send(random.choice(self.laval_quotes))
 
-    @commands.command()
-    @commands.check(is_above_mod)
-    async def twoweeks(self, ctx):
+    async def get_twoweeks(self):
         target_roles = [self.guild.get_role(i) for i in [501092781473792020, 439723470667120640]]
         processed = [] #Returned positive
         for member in self.guild.members:
@@ -63,6 +63,19 @@ class nubby(commands.Cog):
             if any(checks):
                 continue
             processed.append(member)
+        return processed
+
+    @tasks.loop(hours=1)
+    async def check_twoweeks(self):
+        processed = await self.get_twoweeks()
+        if processed:
+            await self.command_channel.send(f"@here there are/is {len(processed)} without required roles use dc!twoweeks to add them")
+
+    @commands.command()
+    @commands.check(is_above_mod)
+    async def twoweeks(self, ctx):
+        target_roles = [self.guild.get_role(i) for i in [501092781473792020, 439723470667120640]]
+        processed = await self.get_twoweeks()
         if not processed:
             return await ctx.send("Everything is balanced, as it should be.")
         msg = "Members that need the roles:\n"+"\n".join([i.name for i in processed])
@@ -90,7 +103,7 @@ class nubby(commands.Cog):
                 processed.remove(target)
             except:
                 await ctx.send("Timed out")
-                break
+                return
         await ctx.send("All roles added")
 
 def setup(bot):
