67865a8ad2fbd1c4d73282aa7be75dc814c2911b
StarrFox <StarrFox6312@gmail.com>
2019-07-02 06:51:15 -0500

    edit processing is in default bot

--- cogs/events.py
+++ cogs/events.py
@@ -16,15 +16,8 @@ class events(commands.Cog):
         """Handles incoming messages"""
         if not self.copycat:
             return
-        if message.channel.id == 381979045090426881:
+        if message.channel.id in [381979045090426881, 381965829857738772]:
             await utils.msg_resend(message, self.copycat)
-        elif message.channel.id == 381965829857738772:
-            await utils.msg_resend(message, self.copycat)
-
-    @commands.Cog.listener()
-    async def on_message_edit(self, before, after):
-        if not after.embeds:
-            await self.bot.process_commands(after)
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
