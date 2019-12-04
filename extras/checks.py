72d5c78036c3ace69cb9ea8ff2bd3788e754a328
StarrFox <43019162+StarrFox@users.noreply.github.com>
2019-12-04 13:29:06 -0600

    update checks

--- extras/checks.py
+++ extras/checks.py
@@ -1,34 +1,29 @@
 from discord.ext import commands
-import discord.utils
-
-def owner_check(ctx):
-    id = ctx.message.author.id
-    return id in ctx.bot.owners
 
 def is_owner():
-    def predicate(ctx):
-        if owner_check(ctx):
+    async def predicate(ctx):
+        if await ctx.bot.is_owner(ctx.author):
             return True
         raise commands.NotOwner()
     return commands.check(predicate)
 
 def has_permissions(**perms):
-    def predicate(ctx):
+    async def predicate(ctx):
         ch = ctx.channel
         permissions = ch.permissions_for(ctx.author)
         missing = [perm for perm, value in perms.items() if getattr(permissions, perm, None) != value]
-        if not missing or owner_check(ctx):
+        if not missing or await ctx.bot.is_owner(ctx.author):
             return True
         raise commands.MissingPermissions(missing)
     return commands.check(predicate)
 
 def guildowner():
-    def predicate(ctx):
+    async def predicate(ctx):
         if ctx.guild is None:
             return False
         elif ctx.message.author == ctx.guild.owner:
             return True
-        elif owner_check(ctx):
+        elif await ctx.bot.is_owner(ctx.author):
             return True
-        raise commands.MissingPermissions(["Server owner"])
+        raise commands.MissingPermissions(["Guild owner"])
     return commands.check(predicate)
