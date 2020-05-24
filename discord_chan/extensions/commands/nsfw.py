e5c2e3df8294a3f4216c1a8bfd6bbb1185bcf7db
StarrFox <starrfox6312@gmail.com>
2020-05-24 17:33:36 -0500

    remove ratings and add nsfw

---
+++ discord_chan/extensions/commands/nsfw.py
@@ -0,0 +1,43 @@
+#  Copyright © 2020 StarrFox
+#
+#  Discord Chan is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Discord Chan is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Discord Chan.  If not, see <https://www.gnu.org/licenses/>.
+
+import discord
+from aiohttp import ClientSession as session
+from discord.ext import commands
+
+NEKO_URL = "https://nekos.life/api/v2/img/lewd"
+
+
+class Nsfw(commands.Cog, name="nsfw"):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    def cog_check(self, ctx: commands.Context):
+        if not ctx.channel.is_nsfw():
+            raise commands.NSFWChannelRequired(ctx.channel)
+
+        return True
+
+    @commands.command()
+    async def neko(self, ctx: commands.Context):
+        """Get a random lewd neko"""
+        async with session() as sess:
+            async with sess.get(NEKO_URL) as res:
+                url = (await res.json())["url"]
+                await ctx.send(embed=discord.Embed().set_image(url=url))
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Nsfw(bot))
