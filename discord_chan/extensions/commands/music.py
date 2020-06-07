b184edae52b158d60728e90d0d63d128449be16b
StarrFox <starrfox6312@gmail.com>
2020-06-06 22:58:44 -0500

    fix otter emoji id

---
+++ discord_chan/extensions/commands/music.py
@@ -0,0 +1,26 @@
+#  Copyright © 2020 StarrFox
+#
+#  Discord Chan is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Discord Chan is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Discord Chan.  If not, see <https://www.gnu.org/licenses/>.
+
+from discord.ext import commands
+
+
+# Todo: finish
+class Music(commands.Cog, name="music"):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Music(bot))
