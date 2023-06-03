d1ac454c5e6d5dc918d4b3db3dc0386492470bc9
StarrFox <StarrFox6312@gmail.com>
2023-06-03 20:22:23 +0000

    add nixos module for discord_chan service

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
+# TODO: add multi-system
 {
   description = "discord chat bot";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     starrpkgs = {
       url = "github:StarrFox/packages";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,70 +15,75 @@
     nixpkgs,
     flake-utils,
     starrpkgs,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        spkgs = starrpkgs.packages.${system};
+  }: let
+    system = "x86_64-linux";
 
-        customOverrides = self: super: {
-          uwuify = super.uwuify.overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or []) ++ [super.poetry];
-            }
-          );
-          attrs = super.attrs.overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or []) ++ [super.hatchling super.hatch-fancy-pypi-readme super.hatch-vcs];
-            }
-          );
-          import-expression = super."import-expression".overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-            }
-          );
-          jishaku = super.jishaku.overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-              propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ [super.setuptools];
-            }
-          );
-          discord-ext-menus = super.discord-ext-menus.overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-            }
-          );
-          "discord-py" = super."discord-py".overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-            }
-          );
-          # this wand version patched the imageMagick library path
-          wand = pkgs.python3Packages.wand;
-        };
+    pkgs = nixpkgs.legacyPackages.${system};
+    spkgs = starrpkgs.packages.${system};
 
-        app = pkgs.poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-          preferWheels = true;
-          overrides = [
-            pkgs.poetry2nix.defaultPoetryOverrides
-            customOverrides
-          ];
-          groups = [
-            "images"
-          ];
-        };
+    customOverrides = self: super: {
+      uwuify = super.uwuify.overridePythonAttrs (
+        old: {
+          buildInputs = (old.buildInputs or []) ++ [super.poetry];
+        }
+      );
+      attrs = super.attrs.overridePythonAttrs (
+        old: {
+          buildInputs = (old.buildInputs or []) ++ [super.hatchling super.hatch-fancy-pypi-readme super.hatch-vcs];
+        }
+      );
+      import-expression = super."import-expression".overridePythonAttrs (
+        old: {
+          buildInputs = (old.buildInputs or []) ++ [super.setuptools];
+        }
+      );
+      jishaku = super.jishaku.overridePythonAttrs (
+        old: {
+          buildInputs = (old.buildInputs or []) ++ [super.setuptools];
+          propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ [super.setuptools];
+        }
+      );
+      discord-ext-menus = super.discord-ext-menus.overridePythonAttrs (
+        old: {
+          buildInputs = (old.buildInputs or []) ++ [super.setuptools];
+        }
+      );
+      "discord-py" = super."discord-py".overridePythonAttrs (
+        old: {
+          buildInputs = (old.buildInputs or []) ++ [super.setuptools];
+        }
+      );
+      # this wand version patched the imageMagick library path
+      wand = pkgs.python3Packages.wand;
+    };
 
-        packageName = "discord_chan";
-      in {
-        packages.${packageName} = app;
+    app = pkgs.poetry2nix.mkPoetryApplication {
+      projectDir = ./.;
+      preferWheels = true;
+      overrides = [
+        pkgs.poetry2nix.defaultPoetryOverrides
+        customOverrides
+      ];
+      groups = [
+        "images"
+      ];
+    };
 
-        defaultPackage = self.packages.${system}.${packageName};
+    packageName = "discord_chan";
+  in {
+    packages.${system} = {
+      ${packageName} = app;
+      default = self.packages.${system}.${packageName};
+    };
 
-        devShell = pkgs.mkShell {
-          name = "discord-chan";
-          packages = with pkgs; [poetry spkgs.commitizen just alejandra black isort];
-        };
-      }
-    );
+    nixosModules = {
+      ${packageName} = import ./modules/discord_chan.nix;
+      default = self.nixosModules.${packageName};
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      name = "discord-chan";
+      packages = with pkgs; [poetry spkgs.commitizen just alejandra black isort];
+    };
+  };
 }
---
+++ modules/discord_chan.nix
@@ -0,0 +1,68 @@
+# TODO: make database name option
+{
+  selfpkgs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.services.discord_chan;
+
+  defaultUser = "discord_chan";
+in {
+  options.services.discord_chan = {
+    enable = mkEnableOption "discord_chan service";
+    user = mkOption {
+      default = defaultUser;
+      example = "alice";
+      type = types.str;
+      description = lib.mdDoc ''
+        Name of an existing user that owns bot process
+        creates a user named discord_chan by default
+      '';
+    };
+    group = mkOption {
+      default = defaultUser;
+      example = "users";
+      type = types.str;
+      description = lib.mdDoc ''
+        Name of an existing group that owns the bot process
+        creates a group named discord_chan by default
+      '';
+    };
+    tokenFile = mkOption {
+      type = types.path;
+      example = literalExpression "~/discord_token";
+      description = lib.mdDoc ''
+        file with discord token to be used by the bot
+      '';
+    };
+  };
+  config = mkIf cfg.enable {
+    systemd.services.discord_chan = {
+      description = "Discord chan bot";
+      wantedBy = ["multi-user.target"];
+      after = ["network-online.target"];
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+        Restart = "always";
+        ExecStart = "${selfpkgs.discord_chan}/bin/discord-chan --secret ${cfg.tokenFile}";
+      };
+    };
+
+    users.users = optionalAttrs (cfg.user == defaultUser) {
+      ${defaultUser} = {
+        description = "discord chan process owner";
+        group = defaultUser;
+      };
+    };
+
+    users.groups = optionalAttrs (cfg.user == defaultUser) {
+      ${defaultUser} = {
+        members = [defaultUser];
+      };
+    };
+  };
+}
