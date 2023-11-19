a59329b8dd6963dabc6514a153cfa8ff9f7c8856
StarrFox <StarrFox6312@gmail.com>
2023-11-19 09:35:09 +0000

    fix feature check and some typing

--- discord_chan/extensions/anime.py
+++ discord_chan/extensions/anime.py
@@ -12,7 +12,7 @@ class Anime(commands.Cog, name="anime"):
     # TODO: figure out what needed to be fixed
     # TODO: fix
     @commands.command(aliases=["sb"])
-    @checks.some_guilds([536702243119038464])
+    @checks.some_guilds(536702243119038464)
     @commands.is_nsfw()
     async def safebooru(self, ctx: SubContext, *tags: str):
         if post := await discord_chan.get_random_safebooru_post(list(tags)):
