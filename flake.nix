a718b74e1307b598465a3da401aa0b65df6d3c15
StarrFox <StarrFox6312@gmail.com>
2023-11-29 10:03:01 +0000

    Revert "use optimized python"
    This reverts commit cb4f879c6b3820dd210eb91f0162768be72f8811.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    starrpkgs.url = "github:StarrFox/packages/main";
     flake-parts.url = "github:hercules-ci/flake-parts/";
     nix-systems.url = "github:nix-systems/default";
   };
@@ -12,7 +11,6 @@
     self,
     flake-parts,
     nix-systems,
-    starrpkgs,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -24,7 +22,7 @@
         self',
         ...
       }: let
-        python = starrpkgs.packages.${system}.python311;
+        python = pkgs.python311;
 
         pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
 
