7c58d06d69f058009042b86b399da7dc49b51549
StarrFox <StarrFox6312@gmail.com>
2023-12-04 12:38:15 +0000

    add anti-olaf behavior

---
+++ discord_chan/extensions/garbage.py
@@ -0,0 +1,30 @@
+import re
+
+import discord
+from discord.ext import commands
+
+import discord_chan
+
+
+loli_filter = re.compile(r"\?tags=(\w|\+)*loli([^a-zA-Z]|$)?")
+
+
+class Garbage(commands.Cog):
+    def __init__(self, bot: discord_chan.DiscordChan) -> None:
+        super().__init__()
+        self.bot = bot
+
+    @commands.Cog.listener("on_message")
+    async def block_olaf_lolis(self, message: discord.Message):
+        if message.guild is None:
+            return
+
+        if message.guild.id != 1015677559020724264:
+            return
+
+        if len(list(loli_filter.finditer(message.content))) > 0:
+            await message.delete()
+
+
+async def setup(bot: discord_chan.DiscordChan):
+    await bot.add_cog(Garbage(bot))
