46dae7776fcafcee8bab169c0a2f58010dee54ae
StarrFox <StarrFox6312@gmail.com>
2024-05-05 15:18:17 -0500

    add safebooru list

--- discord_chan/extensions/anime.py
+++ discord_chan/extensions/anime.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 import discord_chan
 from discord_chan import DiscordChan, SubContext, checks
+from discord_chan.menus import EmbedPageSource, DCMenuPages
 
 
 SAFEBOORU_ALLOWED_GUILDS = [
@@ -18,7 +19,7 @@ class Anime(commands.Cog, name="anime"):
     # TODO: what needed to be fixed??
     # TODO: figure out what needed to be fixed
     # TODO: fix
-    @commands.command(aliases=["sb"])
+    @commands.group(aliases=["sb"], invoke_without_command=True)
     @checks.some_guilds(*SAFEBOORU_ALLOWED_GUILDS)
     @commands.is_nsfw()
     async def safebooru(self, ctx: SubContext, *tags: str):
@@ -28,6 +29,25 @@ class Anime(commands.Cog, name="anime"):
             )
             embed.set_image(url=post.url)
             await ctx.send(embed=embed)
+        else:
+            await ctx.deny("No posts found")
+
+    @safebooru.command(name="list")
+    async def safebooru_list(self, ctx: SubContext, *tags: str):
+        if posts := await discord_chan.get_random_safebooru_posts(list(tags)):
+            embeds: list[discord.Embed] = []
+
+            for post in posts:
+                embed = discord.Embed(
+                    description=f"Post {post.post_index+1}/{post.tag_post_count}"
+                )
+                embed.set_image(url=post.url)
+                embeds.append(embed)
+
+            source = EmbedPageSource(embeds)
+            menu = DCMenuPages(source)
+
+            await menu.start(ctx)
         else:
             await ctx.deny("No posts found")
 
