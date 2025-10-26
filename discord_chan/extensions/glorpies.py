ad191ac7dd1d2cee202749446b4473cb6d877eec
StarrFox <StarrFox6312@gmail.com>
2025-10-26 12:37:17 -0500

    I am become glorp

--- discord_chan/extensions/glorpies.py
+++ discord_chan/extensions/glorpies.py
@@ -1,5 +1,10 @@
+import random
+from datetime import timedelta
+from venv import logger
+
 import discord
 from discord.ext import commands
+from loguru import logger
 
 from discord_chan import (
     DiscordChan,
@@ -16,6 +21,9 @@ class Glorpies(commands.Cog, name="glorpies"):
     def __init__(self, bot: DiscordChan):
         self.bot = bot
 
+        self.landmine_rarity = 20
+
+
     async def cog_check(self, ctx: commands.Context) -> bool:  # type: ignore (this method is allowed to be sync and async)
         if ctx.guild is None:
             raise commands.NoPrivateMessage()
@@ -24,6 +32,28 @@ class Glorpies(commands.Cog, name="glorpies"):
             raise commands.CheckFailure("not glorpy enough")
 
         return True
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if not message.guild or message.guild.id != GLORPY_ID:
+            return
+        
+        should_time = random.randrange(0, self.landmine_rarity) == 1
+
+        if should_time:
+            if not isinstance(message.author, discord.Member):
+                logger.warning("Guild member cache polluted")
+                return
+
+            if not message.channel.permissions_for(message.guild.me).moderate_members:
+                logger.info("Missing permission to timeout members in glorp server")
+                return
+
+            await message.author.timeout(timedelta(minutes=10), reason="sus")
+            await message.channel.send(f"\N{COLLISION SYMBOL} {message.author.mention} stepped on a landmine and has been timed out for 10 years!")
+
+            # progressively more rare
+            self.landmine_rarity += 10
 
     @commands.command(name="rename")
     async def rename_command(
